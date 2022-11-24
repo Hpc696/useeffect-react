2b50a71a548b2d78eeb145022ae1f18f8b32f6a6
MatteoG <honrop.giochi@gmail.com>
2022-11-24 14:11:02 +0100

    use effect con fetch da url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-
+import { useEffect, useState } from 'react';
+import Button from './Button'
 function App() {
-  return (
-    <div>
-      Happy Hacking!
-    </div>
+  
+  const str= ['users', 'posts' , 'photos']
+  
+  const [content, setContent] = useState(str[0])
+
+  const [data , setData]=useState([])
+
+  useEffect(()=>{fetch('https://jsonplaceholder.typicode.com/'+content)
+  .then((response) => response.json())
+  .then((json) => setData(json))}, [content])
+
+  console.log('Componente caricata...')  
+
+  function show(stringhe){
+    setContent(stringhe)
+    console.log('aggiornato')
+  }
+
+  //useEffect( bevo(), [quando mangio, quando ho sete, quando corro,...])
+  //fa 'bevo' quando varia una delle dipendenze 
+
+  
+
+  return (<>
+    <Button azione={() => show(str[0])} nome='Utenti'/>
+    <Button azione={() => show(str[1])} nome='Tweet'/>
+    <Button azione={() => show(str[2])} nome='Commenti'/>
+    <div>{content}</div>
+    <div>{data}</div>
+    </>
   );
 }
 
